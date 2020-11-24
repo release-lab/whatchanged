89ea856f4f2046f7347a5ebd2c9d60e3a3650595
axetroy <axetroy.dev@gmail.com>
2020-11-25 02:58:30 +0800

    fix(repl): production assets should be set publicPath

--- repl/vite.config.js
+++ repl/vite.config.js
@@ -1,6 +1,7 @@
 import httpImport from "import-http/rollup";
 
 export default {
+  base: process.env.NODE_ENV === "production" ? "/changelog" : "/",
   rollupInputOptions: {
     plugins: [httpImport()],
   },
