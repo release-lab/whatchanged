04768c0a7bb06c0e790dc16ce766b56ed01a0a14
axetroy <axetroy.dev@gmail.com>
2020-11-25 03:15:35 +0800

    chore(repl): update vite config

--- repl/vite.config.js
+++ repl/vite.config.js
@@ -2,6 +2,7 @@ import httpImport from "import-http/rollup";
 
 export default {
   base: process.env.NODE_ENV === "production" ? "/changelog" : "/",
+  assetsDir: "./",
   rollupInputOptions: {
     plugins: [httpImport()],
   },
