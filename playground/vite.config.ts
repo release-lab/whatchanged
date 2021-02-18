c13faa11061e22dc359fd6fc040e48752b5cb83b
axetroy <axetroy.dev@gmail.com>
2021-02-18 10:31:36 +0800

    fix: vite config

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -3,9 +3,7 @@ import httpImport from "import-http/rollup";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/whatchanged" : "/",
   root: __dirname,
-  build: {
-    base: process.env.NODE_ENV === "production" ? "/whatchanged" : "/",
-  },
   plugins: [vue(), httpImport()],
 });
