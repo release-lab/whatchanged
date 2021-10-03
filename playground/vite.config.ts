8ecb7d7c269ee8f9a01d6f0c39c3cce7b95e751e
Axetroy <axetroy.dev@gmail.com>
2021-10-03 14:53:09 +0800

    remove playgloud

--- playground/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import vue from "@vitejs/plugin-vue";
-import httpImport from "import-http/rollup";
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/whatchanged" : "/",
-  root: __dirname,
-  plugins: [vue(), httpImport()],
-});
