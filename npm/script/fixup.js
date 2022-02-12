fb86b33646e6d11fc80b6b9192767ab82caf06e5
Axetroy <axetroy.dev@gmail.com>
2022-02-12 19:34:10 +0800

    chore: fix ci

--- npm/script/fixup.js
+++ npm/script/fixup.js
@@ -2,7 +2,7 @@ const path = require("path");
 const fs = require("fs");
 
 function write(filepath, content) {
-  fs.writeFileSync(filepath, content);
+  fs.writeFileSync(filepath, JSON.stringify(content, null, 2));
 }
 
 write(path.join(__dirname, "..", "output", "cjs", "package.json"), {
